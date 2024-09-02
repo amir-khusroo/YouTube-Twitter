80929db63c9e9043133b8f97e5183da82ec94bb1
amir-khusroo <amirkhusroo250@gmail.com>
2024-09-03 00:34:26 +0530

    utils code

--- Backend/src/db/index.js
+++ Backend/src/db/index.js
@@ -4,6 +4,8 @@ import { DB_NAME } from "../constant.js";
 const connectDB=async()=>{
     try {
         const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        //console.log(connectionInstance)
+        // console.log(`Database listening on port ${process.env.PORT}`)
     } catch (error) {
         console.log("MongoDB connection Failed",error);
         process.exit(1);
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,5 +1,14 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({path:'./env'})
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server is listening on port ${process.env.PORT}`);
+    })
+})
+.catch((error)=>{
+    console.log("Database connection failed",error);
+})
